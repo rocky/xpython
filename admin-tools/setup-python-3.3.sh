727f9101ba387510be93b8fce959f460813aba77
rocky <rby@dusytfeet.com>
2024-02-12 09:12:28 -0500

    Admnistrivia

--- admin-tools/setup-python-3.3.sh
+++ admin-tools/setup-python-3.3.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+# Check out python-3.3-to-3.5 and dependent development branches.
+
 PYTHON_VERSION=3.3.7
-pyenv local $PYTHON_VERSION
+
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
 
 function checkout_version {
     local repo=$1
-    version=${2:-python-3.3}
+    version=${2:-python-3.3-to-3.5}
     echo Checking out $version on $repo ...
     (cd ../$repo && git checkout $version && pyenv local $PYTHON_VERSION) && \
 	git pull
@@ -16,16 +23,15 @@ function finish {
   cd $owd
 }
 
-export PATH=$HOME/.pyenv/bin/pyenv:$PATH
 owd=$(pwd)
-bs=${BASH_SOURCE[0]}
-if [[ $0 == $bs ]] ; then
-    echo "This script should be *sourced* rather than run directly through bash"
-    exit 1
-fi
+trap finish EXIT
+
+export PATH=$HOME/.pyenv/bin/pyenv:$PATH
+
 mydir=$(dirname $bs)
 fulldir=$(readlink -f $mydir)
 cd $fulldir/..
 (cd $fulldir/.. && checkout_version python-xdis python-3.3-to-3.5 && checkout_version x-python)
-cd $owd
+
 rm -v */.python-version || true
+finish
