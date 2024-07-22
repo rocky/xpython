8454b2337bc80a07dbbd6e5cde74715164b0b6cc
rocky <rby@dusytfeet.com>
2024-07-22 18:46:02 -0400

    Administrivia

--- admin-tools/merge-for-3.1.sh
+++ admin-tools/merge-for-3.1.sh
@@ -1,5 +1,8 @@
 #/bin/bash
+# Setup for running Python 3.1 .. 3.33, merging Python 3.3-to-3.5 into this branch
+xpython_31_owd=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-3.1.sh; then
     git merge python-3.3-to-3.5
 fi
+cd $xpython_31_owd
