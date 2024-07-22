e45a8f994e653322e993013f03a81440ef441bf8
rocky <rby@dusytfeet.com>
2024-07-22 18:46:02 -0400

    Administrivia

--- admin-tools/merge-for-2.7.sh
+++ admin-tools/merge-for-2.7.sh
@@ -1,5 +1,8 @@
 #/bin/bash
+# Setup for running Python 2.7, merging Python 3.1-to-3.2 into this branch
+xpython_27_owd=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-2.7.sh; then
     git merge python-3.1-to-3.2
 fi
+cd $xpython_27_owd
